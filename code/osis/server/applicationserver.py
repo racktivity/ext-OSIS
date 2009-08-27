f1afaa0c715ff0e5b3c2c08fc4f9c2abe3db1430
nancy <devnull@localhost>
2009-08-27 20:58:57 +0200

    view argument in findAsView is not optional

--- code/osis/server/applicationserver.py
+++ code/osis/server/applicationserver.py
@@ -218,7 +218,7 @@ class OsisServer(BaseServer):
 
 
     @q.manage.applicationserver.expose
-    def findAsView(self, objectType, filters, view=''):
+    def findAsView(self, objectType, filters, view):
         """
         @param objectType: type of the object
         @param filters: filters. list of dicts
