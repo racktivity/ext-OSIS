b6c96dc623dcfeaca454091c83430d2262bbf692
kbonne <devnull@localhost>
2009-11-30 10:43:42 +0100

    STPL-0000 Fixed find issue Reviewer: None * Fixed find issue for local transport overring findAsView

--- code/osis/client/local.py
+++ code/osis/client/local.py
@@ -11,3 +11,21 @@ class LocalTransport(BaseServer):
     Local transport to the Osis server
     """
     
+    def findAsView(self, type_, filter_, view):
+        '''Perform a filter operation on the server
+
+        @param type_: Root object type name
+        @type type_: string
+        @param filter_: Filter definition
+        @type filter_: L{OsisFilterObject}
+        @param view: View to return
+        @type view: string
+
+        @return: List of GUIDs or OsisList of data
+        @rtype: list
+        '''
+        filter_data = filter_.filters
+
+        return BaseServer.findAsView(self, type_, filter_data, view)
+
+    
