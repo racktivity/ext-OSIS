94f54aa88c6c1c70999c5d875a6e658c80e0bec6
Jo De Boeck <jdeboeck@aserver.com>
2012-04-17 17:32:47 +0200

    Use arakoonpoolclient for storing/deleting in osis

--- pyapp/impl/osis/osis/delete/1_object_delete.py
+++ pyapp/impl/osis/osis/delete/1_object_delete.py
@@ -1,6 +1,5 @@
 
 def main(q, i, p, params, tags):
     key  = 'osis.%s.%s.%s'  % (params['domain'], params['rootobjecttype'], params['rootobjectguid'])
-    arakoonClient = q.clients.arakoon.getClient(p.api.appname)
-    if arakoonClient.exists(key): 
-        arakoonClient.delete(key)
+    if p.api.db.exists(key): 
+        p.api.db.delete(key)
--- pyapp/impl/osis/osis/store/3_object_store.py
+++ pyapp/impl/osis/osis/store/3_object_store.py
@@ -6,5 +6,4 @@ def main(q, i, p, params, tags):
     domain = params['domain']
     key  = 'osis.%s.%s.%s'  % (domain, root.PYMODEL_MODEL_INFO.name, root.guid)
     data = ThriftSerializer.serialize(root)
-    arakoonClient = q.clients.arakoon.getClient(p.api.appname)
-    arakoonClient.set(key, data)
+    p.api.db.set(key, data)
