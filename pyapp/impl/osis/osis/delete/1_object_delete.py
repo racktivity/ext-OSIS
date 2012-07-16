9ff9995dab7b1a5111f72dd135d7c850aa4f4093
kneirinc <devnull@localhost>
2012-07-16 11:50:48 +0200

    fixed typo DEVDCPM-1059 / DEVDCPM-988 Reviewed by: rvanover

--- pyapp/impl/osis/osis/delete/1_object_delete.py
+++ pyapp/impl/osis/osis/delete/1_object_delete.py
@@ -1,4 +1,4 @@
 
 def main(q, i, p, params, tags):
     osis = p.application.getOsisConnection(p.api.appname)
-    params['result'] = osis.objectDelete(params['domain']. params['rootobjecttype'], params['rootobjectguid'])
+    params['result'] = osis.objectDelete(params['domain'], params['rootobjecttype'], params['rootobjectguid'])
